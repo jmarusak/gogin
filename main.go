0cd141f2c09df24064b5441764cfa76a8308e0c6
jmarusak <jmarusak@users.noreply.github.com>
2023-07-12 19:54:16 -0400

    large docker image

--- main.go
+++ main.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
 	fmt.Println("Gin Server is running...")
-	r.Run()
 }
